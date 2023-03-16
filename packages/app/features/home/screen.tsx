05e06bdd072f8e55a1d1b2b3ef5b2153675ed7f7
YasBenAll <yassinbenallal@hotmail.com>
2023-03-16 14:44:05 +0100

    App works cross-platform (hell yeahhhhh)

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -9,17 +9,15 @@ export function HomeScreen() {
     <View
       sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
     >
-      <H1 sx={{ fontWeight: '800' }}>Welcome to Solito.</H1>
+      <H1 sx={{ fontWeight: '800' }}>Welcome to Transcribr.</H1>
       <View sx={{ maxWidth: 600 }}>
         <P sx={{ textAlign: 'center' }}>
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
+          Transcribe your favorite podcasts with Transcribr
         </P>
         <P sx={{ textAlign: 'center' }}>
-          Solito is made by{' '}
+          Transcribr is made by{' '}
           <A
-            href="https://twitter.com/fernandotherojo"
+            href="https://twitter.com/yassinius"
             // @ts-expect-error react-native-web only types
             hrefAttrs={{
               target: '_blank',
@@ -27,7 +25,7 @@ export function HomeScreen() {
             }}
             sx={{ color: 'blue' }}
           >
-            Fernando Rojo
+            Yassin Ben Allal
           </A>
           .
         </P>
