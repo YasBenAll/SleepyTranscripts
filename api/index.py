9ba3cf1a75b78f9aa7b10b0a5246a83cd06ed536
YasBenAll <yassinbenallal@hotmail.com>
2023-07-05 19:50:07 +0200

    Connect flask api endpoint with next.js frontend

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/python")
 def hello_world():
     return {"message": 'Hello World'}
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 8080)
