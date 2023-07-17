ccd2e440abccc6c285311b05b327c32550160a92
YasBenAll <yassinbenallal@hotmail.com>
2023-07-17 16:23:52 +0200

    Create MessageBox component + styling

---
+++ src/components/MessageBox.js
@@ -0,0 +1,27 @@
+// MessageBox.js
+
+const MessageBox = ({ type, children }) => {
+    const getTypeStyles = () => {
+      switch (type) {
+        case "success":
+          return "bg-green-100 text-green-800";
+        case "error":
+          return "bg-red-100 text-red-800";
+        case "info":
+          return "bg-blue-100 text-blue-800";
+        default:
+          return "bg-gray-100 text-gray-800";
+      }
+    };
+  
+    const containerClasses = `rounded-md p-4 mb-4 ${getTypeStyles()}`;
+  
+    return (
+      <div className={containerClasses}>
+        <p>{children}</p>
+      </div>
+    );
+  };
+  
+  export default MessageBox;
+  
