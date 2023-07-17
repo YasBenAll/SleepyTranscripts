777dda7b50c72e86893b9255e0f4e361b9d742e4
YasBenAll <yassinbenallal@hotmail.com>
2023-07-17 16:23:58 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// Footer.js
+
+const Footer = () => {
+  return (
+    <footer className="text-center p-4 mt-8">
+      This is the footer. Replace this text with your footer content.
+    </footer>
+  );
+};
+
+export default Footer;
