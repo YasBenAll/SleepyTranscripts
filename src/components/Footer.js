407b8d1f45a59a7526f1c1dea046a7ef7b9f335a
YasBenAll <yassinbenallal@hotmail.com>
2023-07-20 21:43:15 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="text-center p-4 mt-8">
-      Made with <span className="text-red-500">&hearts;</span> for The <Link className="text-blue-500" href="https://www.reddit.com/r/Sleepycabin/"><u>SleepyCabin Subreddit.</u></Link>
+      Made with <span className="text-red-500">&hearts;</span> for The <Link className="text-blue-500" href="https://www.reddit.com/r/Sleepycabin/" target="_blank"><u>SleepyCabin Subreddit.</u></Link>
     </footer>
   );
 };
