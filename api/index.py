563604d18a7fedaf5acc16f9a99fea52a73664ef
YasBenAll <yassinbenallal@hotmail.com>
2023-07-22 22:47:05 +0200

    erf

--- api/index.py
+++ api/index.py
@@ -1,33 +1,34 @@
-# from http.server import BaseHTTPRequestHandler
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
  
-# class handler(BaseHTTPRequestHandler):
  
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header('Content-type','text/plain')
-#         self.end_headers()
-#         self.wfile.write('Hello, world!'.encode('utf-8'))
-#         return
-from flask import Flask
-from flask_cors import CORS
-from readFile import *
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
 
-app = Flask(__name__)
-CORS(app)
 
-with open("episode_metadata.json", "r") as f:
-    episode_metadata = json.load(f)
+# from flask import Flask
+# from flask_cors import CORS
+# from readFile import *
 
-@app.route("/api/episode_data/<episodeName>")
-def episode_data(episodeName):
-    episode = get_episode(episodeName)
-    return episode
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route("/api/episode_name")
-def episode_name():
-    with open("name_list.json", "r", encoding="utf-8") as f:
-        names = json.load(f)
-    return names
+# with open("episode_metadata.json", "r") as f:
+#     episode_metadata = json.load(f)
 
-if __name__ == "__main__":
-    app.run(debug=True, port = 8080)
+# @app.route("/api/episode_data/<episodeName>")
+# def episode_data(episodeName):
+#     episode = get_episode(episodeName)
+#     return episode
+
+# @app.route("/api/episode_name")
+# def episode_name():
+#     with open("name_list.json", "r", encoding="utf-8") as f:
+#         names = json.load(f)
+#     return names
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port = 8080)
