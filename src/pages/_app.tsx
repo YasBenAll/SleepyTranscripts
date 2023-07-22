43547d4c14c0051f6e629f38ff539f80d25353a0
YasBenAll <yassinbenallal@hotmail.com>
2023-07-22 19:44:29 +0200

    Change definition background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,13 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
+    <div className="bg-sleepycabinwoods">
     <Head>
       <link rel="shortcut icon" href="/images/favicon.png" />
       <title>Sleepy Cabin Transcripts</title>
     </Head>
     <Component {...pageProps} />
+    </div>
   </>
   );
 }
