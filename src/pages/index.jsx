ba1ee2aa131e405738e366591060b9e491dcc889
YasBenAll <yassinbenallal@hotmail.com>
2023-07-24 15:59:43 +0200

    Did a loading thing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -82,19 +82,38 @@ export default function Home() {
           </div>
           <div>Episodes:</div>
           {!loading ? (
-        <>
-          <ul className="list-inside mt-4 ">
-            {filteredEpisodes.map((item, i) => (
-              <li key={i} data-members={item.members} className="my-2">
-                <Link href={`/episode/${item.slug}`}>
-                  <div className="text-blue-500 hover:underline">{item.name}</div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
+  <>
+  <ul className="list-inside mt-4 ">
+    {filteredEpisodes.map((item, i) => (
+      <li key={i} data-members={item.members} className="my-2">
+        <Link href={`/episode/${item.slug}`}>
+          <div className="bg-violet-800 border rounded-lg p-4 shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-200 flex items-center justify-center">
+            <div className="text-blue-200 hover:underline text-center">{item.name}</div>
+            {/* Add additional content inside the box if needed */}
+            {/* For example: */}
+            {/* <div className="text-gray-500">{item.description}</div> */}
+          </div>
+        </Link>
+      </li>
+    ))}
+  </ul>
+</>
       ) : (
-        "Loading Transcripts..."
+<div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+  <div class="animate-pulse flex space-x-4">
+    <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-200 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+          <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+        </div>
+        <div class="h-2 bg-slate-200 rounded"></div>
+      </div>
+    </div>
+  </div>
+</div>
       )}
         </div>
       </div>
